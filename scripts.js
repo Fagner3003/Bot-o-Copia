88627bb45e1105cfd17391a8ccbe12119b3f7a02
Fagner Viana <fagner3003analista@gmail.com>
2022-08-20 14:07:33 -0300

    Adicionando arquivo inicial

---
+++ scripts.js
@@ -0,0 +1,9 @@
+<script>
+    function copiarTexto() {
+        let textoCopiado = document.getElementById("texto");
+        textoCopiado.select();
+        textoCopiado.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        alert("O texto é: " + textoCopiado.value);
+    }
+</script>
